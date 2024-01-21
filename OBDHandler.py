9f39e0975ab3f67c116f7488a26a9e15408523f1
lesageethan <lesageethan@gmail.com>
2024-01-21 16:28:47 +0000

    Added some more tweaks to audio levels

--- OBDHandler.py
+++ OBDHandler.py
@@ -33,23 +33,24 @@ class OBDHandler():
 
     def get_pad_volume(self):
         rpm_percent = self.rpm/7000
-        if rpm_percent<0.1:
+        if rpm_percent<0.15:
             return 0
-        return (rpm_percent-0.1)**0.3
+        return (rpm_percent-0.08)*2
     
     def get_bass_volume(self):
         bass_percent = self.accel_pos/100
-        return (bass_percent)**0.1
+        return bass_percent
 
     def get_synth_volume(self):
         synth_percent = self.speed/70
         if synth_percent < 0.17:
             return 0
-        return float(((synth_percent-0.17)**0.3).real)
+        return float(((synth_percent-0.17)**0.7).real)
     
     def get_drums_volume(self):
         drums_percent = self.speed/70
-        if drums_percent < 0.2:
+        drums_percent = -5.76+(41.6*drums_percent)-(64*drums_percent)**2
+        if drums_percent < 0:
             return 0
         return float(((drums_percent-0.20)**0.2).real)
 
@@ -57,20 +58,20 @@ class OBDHandler():
         guitar_percent = self.speed/70
         if guitar_percent < 0.3:
             return 0
-        return float(((guitar_percent-0.30)**0.2).real)
+        return float(((guitar_percent-0.40)**0.2).real)
     
     def get_drums_2_volume(self):
         drums_percent = self.speed/70
         if drums_percent < 0.4:
             return 0
-        return float(((drums_percent-0.40)**0.2).real)
+        return float(((drums_percent-0.45)**0.2).real)
     
     def get_volumes(self):
         volumes = [
-            self.get_bass_volume(),
+            self.get_bass_volume()*0.5,
             self.get_pad_volume(),
-            self.get_synth_volume(),
-            self.get_guitar_volume(),
+            self.get_synth_volume()*0.5,
+            self.get_guitar_volume()*0.5,
             self.get_drums_volume(),
             self.get_drums_2_volume(),
         ]
